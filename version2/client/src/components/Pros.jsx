ecfe00564d08879e1eddf99c5cca16af8c3bb4fb
Ruggy Mango <rans2203@gmail.com>
2024-04-23 23:24:19 +0530

    Modify pro page

--- version2/client/src/components/Pros.jsx
+++ version2/client/src/components/Pros.jsx
@@ -7,358 +7,63 @@ import "../styles/index.scss";
 const Pros = () => {
 	return (
 
+		
 
-		<section>
-			<div class="container max-w-full mx-auto py-24 px-6">
-				<h1
-					class="text-center text-4xl text-black font-medium leading-snug tracking-wider"
-				>
-					Pricing
-				</h1>
-				<p class="text-center text-lg text-gray-700 mt-2 px-6">
-					Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-					accusantium doloremque laudantium, totam rem aperiam.
-				</p>
-				<div
-					class="h-1 mx-auto bg-indigo-200 w-24 opacity-75 mt-4 rounded"
-				></div>
+<div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+<h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Standard plan</h5>
+<div class="flex items-baseline text-gray-900 dark:text-white">
+<span class="text-3xl font-semibold">$</span>
+<span class="text-5xl font-extrabold tracking-tight">49</span>
+<span class="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">/month</span>
+</div>
+<ul role="list" class="space-y-5 my-7">
+<li class="flex items-center">
+<svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+</svg>
+<span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">2 team members</span>
+</li>
+<li class="flex">
+<svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+</svg>
+<span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">20GB Cloud storage</span>
+</li>
+<li class="flex">
+<svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+</svg>
+<span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Integration help</span>
+</li>
+<li class="flex line-through decoration-gray-500">
+<svg class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+</svg>
+<span class="text-base font-normal leading-tight text-gray-500 ms-3">Sketch Files</span>
+</li>
+<li class="flex line-through decoration-gray-500">
+<svg class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+</svg>
+<span class="text-base font-normal leading-tight text-gray-500 ms-3">API Access</span>
+</li>
+<li class="flex line-through decoration-gray-500">
+<svg class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+</svg>
+<span class="text-base font-normal leading-tight text-gray-500 ms-3">Complete documentation</span>
+</li>
+<li class="flex line-through decoration-gray-500">
+<svg class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+</svg>
+<span class="text-base font-normal leading-tight text-gray-500 ms-3">24×7 phone & email support</span>
+</li>
+</ul>
+<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Choose plan</button>
+</div>
 
-				<div class="max-w-full md:max-w-6xl mx-auto my-3 md:px-8">
-					<div
-						class="relative block flex flex-col md:flex-row items-center"
-					>
-						<div
-							class="w-11/12 max-w-sm sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-mr-4"
-						>
-							<div
-								class="bg-white text-black rounded-lg shadow-inner shadow-lg overflow-hidden"
-							>
-								<div
-									class="block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
-								>
-									<h1
-										class="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
-									>
-										Hobby
-									</h1>
-									<h2 class="text-sm text-gray-500 text-center pb-6">FREE</h2>
-
-									Stripe offers everything needed to run an online business
-									at scale. Get in touch for details.
-								</div>
-
-								<div class="flex flex-wrap mt-3 px-6">
-									<ul>
-										<li class="flex items-center">
-											<div
-												class=" rounded-full p-2 fill-current text-green-700"
-											>
-												<svg
-													class="w-6 h-6 align-middle"
-													width="24"
-													height="24"
-													viewBox="0 0 24 24"
-													fill="none"
-													stroke="currentColor"
-													stroke-width="2"
-													stroke-linecap="round"
-													stroke-linejoin="round"
-												>
-													<path
-														d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-													></path>
-													<polyline
-														points="22 4 12 14.01 9 11.01"
-													></polyline>
-												</svg>
-											</div>
-											<span class="text-gray-700 text-lg ml-3"
-											>No setup</span
-											>
-										</li>
-										<li class="flex items-center">
-											<div
-												class=" rounded-full p-2 fill-current text-green-700"
-											>
-												<svg
-													class="w-6 h-6 align-middle"
-													width="24"
-													height="24"
-													viewBox="0 0 24 24"
-													fill="none"
-													stroke="currentColor"
-													stroke-width="2"
-													stroke-linecap="round"
-													stroke-linejoin="round"
-												>
-													<path
-														d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-													></path>
-													<polyline
-														points="22 4 12 14.01 9 11.01"
-													></polyline>
-												</svg>
-											</div>
-											<span class="text-gray-700 text-lg ml-3"
-											>No setups</span
-											>
-										</li>
-										<li class="flex items-center">
-											<div
-												class=" rounded-full p-2 fill-current text-green-700"
-											>
-												<svg
-													class="w-6 h-6 align-middle"
-													width="24"
-													height="24"
-													viewBox="0 0 24 24"
-													fill="none"
-													stroke="currentColor"
-													stroke-width="2"
-													stroke-linecap="round"
-													stroke-linejoin="round"
-												>
-													<path
-														d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-													></path>
-													<polyline
-														points="22 4 12 14.01 9 11.01"
-													></polyline>
-												</svg>
-											</div>
-											<span class="text-gray-700 text-lg ml-3">Speed</span>
-										</li>
-									</ul>
-								</div>
-								<div class="block flex items-center p-8  uppercase">
-									<button
-										class="mt-3 text-lg font-semibold 
-	bg-black w-full text-white rounded-lg 
-	px-6 py-3 block shadow-xl hover:bg-gray-700"
-									>
-										Select
-									</button>
-								</div>
-							</div>
-						</div>
-						<div
-							class="w-full max-w-md sm:w-2/3 lg:w-1/3 sm:my-5 my-8 relative z-10 bg-white rounded-lg shadow-lg"
-						>
-							<div
-								class="text-sm leading-none rounded-t-lg bg-gray-200 text-black font-semibold uppercase py-4 text-center tracking-wide"
-							>
-								Most Popular
-							</div>
-							<div
-								class="block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
-							>
-								<h1
-									class="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
-								>
-									Expert
-								</h1>
-								<h2 class="text-sm text-gray-500 text-center pb-6"><span class="text-3xl">€19</span> /mo</h2>
-
-								Stripe offers everything needed to run an online business at
-								scale. Get in touch for details.
-							</div>
-							<div class="flex pl-12 justify-start sm:justify-start mt-3">
-								<ul>
-									<li class="flex items-center">
-										<div
-											class="rounded-full p-2 fill-current text-green-700"
-										>
-											<svg
-												class="w-6 h-6 align-middle"
-												width="24"
-												height="24"
-												viewBox="0 0 24 24"
-												fill="none"
-												stroke="currentColor"
-												stroke-width="2"
-												stroke-linecap="round"
-												stroke-linejoin="round"
-											>
-												<path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-												<polyline points="22 4 12 14.01 9 11.01"></polyline>
-											</svg>
-										</div>
-										<span class="text-gray-700 text-lg ml-3">No setup</span>
-									</li>
-									<li class="flex items-center">
-										<div
-											class=" rounded-full p-2 fill-current text-green-700"
-										>
-											<svg
-												class="w-6 h-6 align-middle"
-												width="24"
-												height="24"
-												viewBox="0 0 24 24"
-												fill="none"
-												stroke="currentColor"
-												stroke-width="2"
-												stroke-linecap="round"
-												stroke-linejoin="round"
-											>
-												<path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-												<polyline points="22 4 12 14.01 9 11.01"></polyline>
-											</svg>
-										</div>
-										<span class="text-gray-700 text-lg ml-3"
-										>Hidden fees</span
-										>
-									</li>
-									<li class="flex items-center">
-										<div
-											class=" rounded-full p-2 fill-current text-green-700"
-										>
-											<svg
-												class="w-6 h-6 align-middle"
-												width="24"
-												height="24"
-												viewBox="0 0 24 24"
-												fill="none"
-												stroke="currentColor"
-												stroke-width="2"
-												stroke-linecap="round"
-												stroke-linejoin="round"
-											>
-												<path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-												<polyline points="22 4 12 14.01 9 11.01"></polyline>
-											</svg>
-										</div>
-										<span class="text-gray-700 text-lg ml-3">Original</span>
-									</li>
-								</ul>
-							</div>
-
-							<div class="block flex items-center p-8  uppercase">
-								<button
-									class="mt-3 text-lg font-semibold 
-	bg-black w-full text-white rounded-lg 
-	px-6 py-3 block shadow-xl hover:bg-gray-700"
-								>
-									Select
-								</button>
-							</div>
-						</div>
-						<div
-							class="w-11/12 max-w-sm sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-ml-4"
-						>
-							<div
-								class="bg-white text-black rounded-lg shadow-inner shadow-lg overflow-hidden"
-							>
-								<div
-									class="block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
-								>
-									<h1
-										class="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
-									>
-										Enteprise
-									</h1>
-									<h2 class="text-sm text-gray-500 text-center pb-6">€39 /mo</h2>
-
-									Stripe offers everything needed to run an online business
-									at scale. Get in touch for details.
-								</div>
-								<div class="flex flex-wrap mt-3 px-6">
-									<ul>
-										<li class="flex items-center">
-											<div
-												class=" rounded-full p-2 fill-current text-green-700">
-												<svg
-													class="w-6 h-6 align-middle"
-													width="24"
-													height="24"
-													viewBox="0 0 24 24"
-													fill="none"
-													stroke="currentColor"
-													stroke-width="2"
-													stroke-linecap="round"
-													stroke-linejoin="round"
-												>
-													<path
-														d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-													></path>
-													<polyline
-														points="22 4 12 14.01 9 11.01"></polyline>
-												</svg>
-											</div>
-											<span class="text-gray-700 text-lg ml-3"
-											>Electric</span>
-										</li>
-										<li class="flex items-center">
-											<div
-												class=" rounded-full p-2 fill-current text-green-700"
-											>
-												<svg
-													class="w-6 h-6 align-middle"
-													width="24"
-													height="24"
-													viewBox="0 0 24 24"
-													fill="none"
-													stroke="currentColor"
-													stroke-width="2"
-													stroke-linecap="round"
-													stroke-linejoin="round"
-												>
-													<path
-														d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-													></path>
-													<polyline
-														points="22 4 12 14.01 9 11.01"
-													></polyline>
-												</svg>
-											</div>
-											<span class="text-gray-700 text-lg ml-3"
-											>Monthly</span
-											>
-										</li>
-										<li class="flex items-center">
-											<div
-												class=" rounded-full p-2 fill-current text-green-700"
-											>
-												<svg
-													class="w-6 h-6 align-middle"
-													width="24"
-													height="24"
-													viewBox="0 0 24 24"
-													fill="none"
-													stroke="currentColor"
-													stroke-width="2"
-													stroke-linecap="round"
-													stroke-linejoin="round"
-												>
-													<path
-														d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-													></path>
-													<polyline
-														points="22 4 12 14.01 9 11.01"
-													></polyline>
-												</svg>
-											</div>
-											<span class="text-gray-700 text-lg ml-3"
-											>No setup</span
-											>
-										</li>
-									</ul>
-								</div>
-
-								<div class="block flex items-center p-8  uppercase">
-									<button
-										class="mt-3 text-lg font-semibold 
-	bg-black w-full text-white rounded-lg 
-	px-6 py-3 block shadow-xl hover:bg-gray-700"
-									>
-										Select
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+		
 
 
 	)
--- version2/client/src/components/Services.jsx
+++ version2/client/src/components/Services.jsx
@@ -414,7 +414,7 @@ const Services = () => {
             <a href="#">
               <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">This will look good with your selected cloth</h5>
             </a>
-            <div class="flex items-center mt-2.5 mb-5">
+            {/* <div class="flex items-center mt-2.5 mb-5">
               <div class="flex items-center space-x-1 rtl:space-x-reverse">
                 <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                   <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -433,10 +433,10 @@ const Services = () => {
                 </svg>
               </div>
               <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">4.0</span>
-            </div>
+            </div> */}
             <div class="flex items-center justify-between">
               <span class="text-3xl font-bold text-gray-900 dark:text-white"></span>
-              <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleRecommendationNext}>Next Recommendation</a>
+              <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleRecommendationNext}>Next Suggestion</a>
             </div>
           </div>
         </div>
@@ -461,7 +461,7 @@ const Services = () => {
                   <p className='font-bold'>{image.clothType}</p>
                 </div>
                 <div className="w-full flex pb-4 px-4 items-end justify-between">
-                  <button onClick={() => { handleRecommend(image.imageName, image.clothType, image.extra) }} className='bg-blue-500 w-[140px] text-white px-4 py-2 rounded-lg'>Recommend</button>
+                  <button onClick={() => { handleRecommend(image.imageName, image.clothType, image.extra) }} className='bg-blue-500 w-[140px] text-white px-4 py-2 rounded-lg'>Suggest</button>
                   <button onClick={() => handleDelete(image.imageName)} className='bg-red-500 text-white px-4 py-2 rounded-lg'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
