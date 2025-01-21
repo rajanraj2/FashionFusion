1f82b81b0731687a10821839597b143800594c2f
rajanraj2 <rajanraj253545@gmail.com>
2025-01-21 19:50:02 +0530

    Change file names

--- version2/client/src/main.jsx
+++ version2/client/src/main.jsx
@@ -6,7 +6,10 @@ import { AuthProvider } from './store/auth.jsx'
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
-
+// app.use(express.static("client/build"))
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+// });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
--- version2/server/model.py
+++ version2/server/model.py
@@ -1,8 +1,8 @@
 import sys
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import cv2
-import os
+# import cv2
+# import os
 import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
